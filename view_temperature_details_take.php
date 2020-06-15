cba0ab48b5b21b26094f9a333a92d830adab2427
naposam <samnapoleon86@gmail.com>
2020-06-15 09:52:23 +0000

    new update

--- view_temperature_details_take.php
+++ view_temperature_details_take.php
@@ -11,6 +11,11 @@
 			background-repeat: no-repeat!important;
 			background-size: cover!important;
 		}
+		@media print {
+    #printbtn {
+       margin-bottom: 2em;
+    }
+}
 	</style>
 </head>
     <body>
@@ -41,7 +46,7 @@ $sql="SELECT * FROM  members_registration WHERE uid='$get_id'";
                             <div class="block-content collapse in">
                       <div class="span12">      	
            <table cellpadding="0" cellspacing="0" border="1" class="table" id="">
-            <div class="alert alert-success text-center"><?php echo strtoupper($data['fname']).' '.strtoupper($data['mname']).' '.strtoupper($data['lname']) ;?> </div>
+            <div class="alert alert-success text-center" id="printbtn"><?php echo strtoupper($data['fname']).' '.strtoupper($data['mname']).' '.strtoupper($data['lname']) ;?> </div>
 		<thead>
 		<tr>
 					<th>#ID</th>
@@ -75,7 +80,67 @@ $sql="SELECT * FROM  members_registration WHERE uid='$get_id'";
 		                </tbody>
 	                     </table>
 						</div>
-                      
+						<br><br><br><br>
+                     <div class="span12">      	
+           <table cellpadding="0" cellspacing="0" border="1" class="table" id="">
+            <div class="alert alert-success text-center" id="printbtn">QUESTIONNAIRE </div>
+		<thead>
+		<tr>
+					<th>#ID</th>
+					<th>History Of Cough</th>
+					<th>Fever</th>
+					<th>Flu</th>
+					<th>Difficulty Breathing</th>
+					<th>Travel Outside</th>
+					<th>Country Visited</th>
+					<th>Travell Outside Town</th>
+					<th>Region/Town Visited</th>
+					<th>Exposed Covid 19</th>
+					<th>Friend with Fever or Cough</th>
+					<th>Wort at Hospital</th>
+					<th>Name Hospital Worked </th>
+					<th>DATE</th>
+					<th>TIME</th>
+					
+					
+		</tr>
+		</thead>
+												<?php
+				$sql="SELECT * FROM  qustion_tbl WHERE uid='$get_id'";
+                $result=mysqli_query($mysqli,$sql);
+                $i = 0;
+                 while($row=mysqli_fetch_array($result)){
+                  $i++;
+                $datepaid=date('Y-m-d', strtotime($row['date_record']));
+                // $time=date('H:i A',strtotime($row['date_record']));
+                 
+						?>
+						
+						
+						<tr>
+		               <td><?php echo $i; ?></td>
+		                <td><?php echo $row['history_dry_cough']; ?></td> 
+		                <td><?php echo $row['fever']; ?></td> 
+		                <td><?php echo $row['flu']; ?></td> 
+		                <td><?php echo $row['difficulty_breathing']; ?></td> 
+		                <td><?php echo $row['travelled_outside_gh']; ?></td> 
+		                <td><?php echo $row['country_visited']; ?></td> 
+		                <td><?php echo $row['travelled_outside_town']; ?></td> 
+		                <td><?php echo $row['specify_region']; ?></td> 
+		                <td><?php echo $row['exposed_covid']; ?></td> 
+		                <td><?php echo $row['friend_relative_fever']; ?></td> 
+		                <td><?php echo $row['working_hospital']; ?></td> 
+		                <td><?php echo $row['hospital_worked']; ?></td>  
+		               <td><?php echo $datepaid ?></td>
+		               <!-- <td><?php echo $time; ?></td>   -->
+		              
+		               
+		               </tr>
+   
+	               <?php } ?> 
+		                </tbody>
+	                     </table>
+						</div>
 						<br><br><br><br>
 						
 
